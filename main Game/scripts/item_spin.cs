d27491b39dababa197853cc905bb8162d66f4372
GhostTales <120366210+GhostTales@users.noreply.github.com>
2023-10-09 13:31:00 +0200

    item spin animation updated

--- main Game/scripts/item_spin.cs
+++ main Game/scripts/item_spin.cs
@@ -1,32 +1,13 @@
 using Godot;
 using System;
-using System.Collections.Generic;
 
-public partial class item_spin : Node2D
+public partial class item_spin : Area2D
 {
-	// Called when the node enters the scene tree for the first time.
-	private AnimatedSprite2D anim;
-	private AudioStreamPlayer2D pick_up;
-	public int item_spin_animation = 2;
-	private List<string> animations = new List<string>() { "gem_spin", "silver_coin_spin", "gold_coin_spin" };
-	public override void _Ready()
-	{
-		anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		pick_up = GetNode<AudioStreamPlayer2D>("pick_up");
-		pick_up.Play();
-	}
 
-	public void Set_Spin_Anim(int n) { item_spin_animation = n; }
+    private void _on_body_entered(Node2D body)
+    {
+        this.QueueFree();
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
 
-		if (item_spin_animation < animations.Count)
-		{
-			anim.Play(animations[item_spin_animation]);
-		}
-		else
-			GD.Print($"error item_spin_animation ({item_spin_animation}) out of index ({animations.Count - 1})");
-	}
 }
