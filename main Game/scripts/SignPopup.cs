e515b166e64334fbeaf90090f7bd2b7c2d8036e2
GhostTales <120366210+GhostTales@users.noreply.github.com>
2023-10-13 08:22:49 +0200

    added sign popup

---
+++ main Game/scripts/SignPopup.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class SignPopup : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		this.Visible = false;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	private void _on_body_entered(CharacterBody2D body)
+	{
+		this.Visible = true;
+		GD.Print(body);
+	}
+
+	private void _on_body_exited(CharacterBody2D body)
+	{
+		this.Visible = false;
+		GD.Print("hide");
+	}
+
+}
