ff8fa1ac90cb8235e2da065a0acd576006a0c18b
GhostTales <120366210+GhostTales@users.noreply.github.com>
2023-10-30 12:53:02 +0100

    Update TrykPlade.cs

--- main Game/scripts/TrykPlade.cs
+++ main Game/scripts/TrykPlade.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 
 public partial class TrykPlade : Area2D
@@ -7,6 +8,7 @@ public partial class TrykPlade : Area2D
 
 	public bool CurrentState { get; private set; }
 	public AnimatedSprite2D texture;
+	public List<Node2D> _overlapping = new List<Node2D>();
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -19,12 +21,18 @@ public partial class TrykPlade : Area2D
 	{
 		texture.Play("on");
 		CurrentState = true;
+		_overlapping.Add(body);
 	}
 
 	private void _on_body_exited(Node2D body)
 	{
-		texture.Play("off");
-		CurrentState = false;
+		if (_overlapping.Count == 1)
+		{
+			texture.Play("off");
+			CurrentState = false;
+		}
+
+		_overlapping.Remove(body);
 	}
 
 }
