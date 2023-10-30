7187c224f9ca88801cdf96ae8858a4c6d84c3812
GhostTales <120366210+GhostTales@users.noreply.github.com>
2023-10-30 12:01:30 +0100

    couples tryplade op til door

--- main Game/scripts/door.cs
+++ main Game/scripts/door.cs
@@ -1,22 +1,28 @@
 using Godot;
-using System;
 
 public partial class door : StaticBody2D
 {
-	private const int Off = 1;
-	private const int On = 2;
-	private int CurrentState = Off;
-	
+	private bool CurrentState;
+	private Sprite2D door_sprite;
+	private TrykPlade TrykPlade;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	
+		door_sprite = GetNode<Sprite2D>("Sprite2D");
+		TrykPlade = GetNode<TrykPlade>($"{this.GetParent().GetPath()}/TrykPlade");
 	}
-	
+
 	public override void _PhysicsProcess(double delta)
 	{
-		if( CurrentState == Off) {
-			
+		CurrentState = TrykPlade.CurrentState;
+		if (CurrentState == false)
+		{
+			door_sprite.Texture = (Texture2D)GD.Load("res://art/objects/chest_01.png");
+		}
+		else
+		{
+			door_sprite.Texture = (Texture2D)GD.Load("res://art/objects/coin_silver.png");
 		}
 	}
 }
