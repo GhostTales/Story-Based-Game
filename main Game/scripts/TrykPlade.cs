f8dfd84524b9c71edc25aee55ac7a50aa233eff7
GhostTales <120366210+GhostTales@users.noreply.github.com>
2023-10-30 10:52:40 +0100

    cleaned up trykplade and made currentstate public

--- main Game/scripts/TrykPlade.cs
+++ main Game/scripts/TrykPlade.cs
@@ -4,28 +4,27 @@ using System;
 
 public partial class TrykPlade : Area2D
 {
-	private const int Off = 1;
-	private const int On = 2;
-	private int CurrentState = Off;
+
+	public bool CurrentState { get; private set; }
 	public AnimatedSprite2D texture;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		texture = GetNode<AnimatedSprite2D>("CollisionShape2D/AnimatedSprite2D");
-CurrentState = Off;
+		CurrentState = false;
 		texture.Play("off");
 	}
 
 	private void _on_body_entered(Node2D body)
 	{
 		texture.Play("on");
-CurrentState = On;
+		CurrentState = true;
 	}
 
 	private void _on_body_exited(Node2D body)
 	{
 		texture.Play("off");
-CurrentState = Off;
+		CurrentState = false;
 	}
 
 }
